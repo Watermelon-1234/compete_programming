2fac506108888893375292612e8699486ff78249
idoit_melon <948794harvey@gmail.com>
2025-12-10 10:19:47 +0800

    sync mac with remote

--- 12th_grade/a118.cpp
+++ 12th_grade/a118.cpp
@@ -1,7 +1,5 @@
 #pragma GCC optimize("O4,unroll-loops")
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
 #define fastio ios_base::sync_with_stdio(false),cin.tie(0);
 #define safe cerr << "\e[1;31m" << __PRETTY_FUNCTION__ << " line " << __LINE__ << " safe\e[0m\n";
 #define int int64_t
@@ -15,10 +13,7 @@
 #define oof(i,a,b) for(i=(a);i>=(b);--i)
 #define all(x) begin(x),end(x)
 #define btw(a,b,c) ((a)<=(b)&&(b)<=(c))
-using namespace __gnu_pbds;
 using namespace std;
-typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_set;
-typedef tree<int,null_type,less_equal<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
 
 template <class ...T> void debug(T ...args)
 {
@@ -41,6 +36,7 @@ T& inpr()
 }
 
 int n,x;
+bool solved = 0;
 
 int fast_pow(int base,int times)
 {
@@ -50,37 +46,53 @@ int fast_pow(int base,int times)
     }
     else if(times&1)
     {
-        return fast_pow(base,time-1) * bast;
+        return fast_pow(base,times-1) * base;
     }
     else
     {
-        return fast_pow(base,time/2) * fast_pow(base,time/2);
+        return fast_pow(base,times/2) * fast_pow(base,times/2);
     }
 }
 
-void compatible(int f)
-{
-    int t = sqrt(f);
-    if(f == t*t)
-    {
-        return 1;
-    }
-    return 0;
-}
+// void compatible(int f)
+// {
+//     int t = sqrt(f);
+//     if(f == t*t)
+//     {
+//         return 1;
+//     }
+//     return 0;
+// }
 
-vector<int> sol(int sum,int cnt,vector<int> history)
+void sol(int sum,int cnt,int pick,vector<int> history)
 {
-    
+    sum -= pick * pick;
+    // debug(sum,cnt,pick);
     if(cnt == 4)
     {
-        if(compatible(sum))
+        // safe;
+        if(sum == 0)
         {
-            history.push_back(sqrt(sum));
+            solved = 1;
+            for(auto i:history)
+            {
+                cout<<i<<" ";
+            }
+            cout<<'\n';
         }
     }
-    else
+    else if(sum > 0)
     {
-        for(int )
+        if(sum < pick * pick)
+        {
+            return;
+        }
+        // safe;
+        for(int i = pick; i <= sqrt(sum); i++)
+        {
+            history.push_back(i);
+            sol(sum, cnt + 1, i, history);
+        }
     }
 }
 
@@ -90,5 +102,14 @@ int main()
     #define int int64_t
     fastio;
     n = inpr<int>();
-    vector<int> ans = sol(fast_pow(2,n),1,vector<int> a());
+    for(int i = 1;i < fast_pow(2,(n%2?n:n+1)/2);i++)
+    {
+        vector<int> a;
+        a.push_back(i);
+        sol(fast_pow(2,n),1,i,a);
+    }
+    if(!solved)
+    {
+        cout<<0<<"\n";
+    }
 }
